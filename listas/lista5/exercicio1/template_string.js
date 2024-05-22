0c41f242fddbada1dc70ef81d2c001493d391a77
faelzin011 <rafaelvasonceloscoutinho@gmail.com>
2024-05-22 09:26:53 -0300

    Subindo a lista 5 de desenvolvimento-web

---
+++ listas/lista5/exercicio1/template_string.js
@@ -0,0 +1,6 @@
+var nome = "Rafael"
+var idade = 18 
+var emprego = "Estudante do ifsp"
+var endereco = "Rua Londrina"
+
+alert(`Seu nome é ${nome} \n Voce tem ${idade} anos \n Seu emprego atual é: ${emprego} \n E mora no endereco: ${endereco}`)
---
+++ listas/lista5/exercicio2/ex02.js
@@ -0,0 +1,6 @@
+var nome = "Rafael"
+var idade = 18
+var emprego = "Estudante do IFSP"
+var endereco = "Rua Londrina"
+
+console.log(`Seu nome é ${nome} \nVoce tem ${idade} anos\nSeu emprego atual é: ${emprego} \nE seu emprego atual é: ${endereco}`)

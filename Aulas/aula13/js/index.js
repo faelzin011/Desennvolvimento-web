273c73676451b4e64b6a08898ab2b1d0a1eb4e14
fael011 <rafaelvasconceloscoutinho@gmail.com>
2024-07-13 11:43:30 -0300

    subiando a aula

---
+++ Aulas/aula13/js/index.js
@@ -0,0 +1,19 @@
+let conteudoPrincipal = document.getElementById('conteudo-principal')
+let cep = document.getElementById('cep')
+
+const buscarCEP = () => {
+  fetch(`https://viacep.com.br/ws/${cep.value}/json/`, {
+    methods: 'GET',
+  }).then(resposta => resposta.json())
+    .then(dados => {
+      conteudoPrincipal.innerHTML = `
+      <h1>Endereço: </h1>
+      <p>${dados.cep}<p>
+      <p>${dados.logradouro}<p>
+      <p>${dados.bairro}<p>
+      <p>${dados.localidade} - ${dados.uf}<p>
+      `
+    })
+
+}
+
